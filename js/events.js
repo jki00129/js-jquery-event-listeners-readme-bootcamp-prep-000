23993f39c247b4e643323db0c6ce5a1bf4c113f1
Jeewon Kim <jki00129@gmail.com>
2018-02-06 23:41:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
-//define functions here
+//define functions herefunction getIt()
+function getIt(){
 
 $(document).ready(function(){
 
 // call functions here
-
+alert("Hey!");
+  return;
 });
+}
+function frameIt(){
+  $('img').on('load', function(){
+    
+  return;
+});
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+  alert ("Your form is going to be submitted now.")
+  };
+});
+}
