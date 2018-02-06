30669539c8bb48da555bedef70a509f0ee1ef072
Jeewon Kim <jki00129@gmail.com>
2018-02-06 23:48:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,15 +9,8 @@ alert("Hey!");
 });
 }
 function frameIt(){
-  $('tasty').on('load', function(){
+  $('.tasty').on('load', function(){
   return;
 });
 }
 
-function submitIt(){
-  $("form").on("submit", function() {
-  if ($( "input:first" ).val() === "correct") {
-  alert ("Your form is going to be submitted now.")
-  };
-});
-}
