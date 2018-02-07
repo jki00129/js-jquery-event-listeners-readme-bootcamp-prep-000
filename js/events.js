eb589d8b8638b84836c52499626784e620073261
Jeewon Kim <jki00129@gmail.com>
2018-02-07 00:11:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,6 @@ function submitIt(){
   $("form").on("submit", function() {
   if ($( "input:first" ).val() === "correct") {
     alert("Your form is going to be submitted now.");
-        window.submitIt();
     return;
   }
 });
